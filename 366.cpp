ccf1c2f34fb41e10c09d3cba5601cae2e1f3cebc
arjacent <arjacent@hotmail.com>
2020-07-20 22:23:57 -0400

    Shaving trees off binary tree

---
+++ 366.cpp
@@ -0,0 +1,36 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+  vector<vector<int>> findLeaves(TreeNode* root) {
+    vector<vector<int>> ans = {};
+    vector<int>* inner;
+    while (root != nullptr) {
+      inner = new vector<int>;
+      root = trimLeaves(inner, root);
+      ans.push_back(*inner);
+    }
+    return ans;
+  }
+  
+  TreeNode* trimLeaves(vector<int>* ans, TreeNode* root) {
+    if (root == nullptr) return nullptr;
+    if (root->left == nullptr && root->right == nullptr) {
+      ans->push_back(root->val);
+      return nullptr;
+    }
+    root->left = trimLeaves(ans, root->left);
+    root->right = trimLeaves(ans, root->right);
+    return root;
+  }
+  
+};
